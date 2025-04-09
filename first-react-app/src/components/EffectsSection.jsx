a3f8216484b6136cfedcf50204ba7ecceae3b96b
SSH1van <akaunt654321@gmail.com>
2025-04-09 15:31:05 +0300

    Imitation loading data from server, using useEffect

--- first-react-app/src/components/EffectsSection.jsx
+++ first-react-app/src/components/EffectsSection.jsx
@@ -1,18 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "./Button/Button.jsx";
 import Modal from "./Modal/Modal.jsx";
 
 export default function EffectSection() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
 
-  function openModal() {
-    setIsModalOpen(true);
+  async function fetchUsers() {
+    setLoading(true);
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+    console.log(users);
+    setUsers(users);
+    setLoading(false);
   }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
   return (
     <section>
       <h3>Эффекты</h3>
-      <Button style={{width: "250px"}} onClick={openModal}>Открыть информацию</Button>
+      <Button
+        style={{ marginBottom: "1rem" }}
+        onClick={() => setIsModalOpen(true)}
+      >
+        Открыть информацию
+      </Button>
       <Modal open={isModalOpen}>
         <h3>Hello from modal</h3>
         <p>
@@ -23,7 +39,16 @@ export default function EffectSection() {
         </p>
         <Button onClick={() => setIsModalOpen(false)}>Close modal</Button>
       </Modal>
-      
+
+      {loading && <p>Loading...</p>}
+
+      {!loading && (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      )}
     </section>
   );
 }
