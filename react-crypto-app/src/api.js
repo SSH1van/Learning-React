9cd0886a9d244de4120ed22bdbb1690a98993ca3
SSH1van <akaunt654321@gmail.com>
2025-04-11 17:26:55 +0300

    Fix api.js

--- react-crypto-app/src/api.js
+++ react-crypto-app/src/api.js
@@ -1,17 +1,17 @@
 import { cryptoAssets, cryptoData } from "./data";
 
-export default function fakeFetchCrypto() {
+export function fakeFetchCrypto() {
     return new Promise(resolve => {
         setTimeout(() => {
             resolve(cryptoData)
-        }, 2000)
+        }, 1)
     })
 }
 
-export default function fetchAssets() {
+export function fetchAssets() {
     return new Promise(resolve => {
         setTimeout(() => {
             resolve(cryptoAssets)
-        }, 2000)
+        }, 1)
     })
 }
