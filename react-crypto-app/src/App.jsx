a4257d08a3da72c20507ea22b87783c642335160
SSH1van <akaunt654321@gmail.com>
2025-04-16 11:15:57 +0300

    Create AppLayout.jsx, refactor components App.jsx and AppSlider.jsx

--- react-crypto-app/src/App.jsx
+++ react-crypto-app/src/App.jsx
@@ -1,19 +1,10 @@
-import { Layout } from "antd";
-import AppHeader  from "./components/layout/AppHeader";
-import AppSlider from "./components/layout/AppSlider";
-import AppContent from "./components/layout/AppContent";
 import { CryptoContextProvider } from "./context/crypto-context";
+import AppLayout from "./components/layout/AppLayout";
 
 export default function App() {
   return (
     <CryptoContextProvider>
-      <Layout>
-        <AppHeader />
-        <Layout>
-          <AppSlider />
-          <AppContent />
-        </Layout>
-      </Layout>
+      <AppLayout />
     </CryptoContextProvider>
   );
 }
--- react-crypto-app/src/components/layout/AppSlider.jsx
+++ react-crypto-app/src/components/layout/AppSlider.jsx
@@ -1,4 +1,4 @@
-import { Layout, Card, Statistic, List, Typography, Spin, Tag } from "antd";
+import { Layout, Card, Statistic, List, Typography, Tag } from "antd";
 import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
 import { capitalize } from "../../utils.js";
 import { useContext } from "react";
@@ -9,11 +9,7 @@ const siderStyle = {
 };
 
 export default function AppSlider() {
-  const { loading, assets } = useContext(CryptoContext);
-
-  if (loading) {
-    return <Spin fullscreen />;
-  }
+  const { assets } = useContext(CryptoContext);
 
   return (
     <Layout.Sider width="25%" style={siderStyle}>
